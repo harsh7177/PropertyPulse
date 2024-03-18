67603b94af9e4390aa5f352498ef7258b54f03b7
Harsh Kandari <harshsingh7177@gmail.com>
2024-03-18 16:57:12 +0530

    Update scrapping.py

--- scrapping.py
+++ scrapping.py
@@ -9,10 +9,7 @@ import json
 
 @st.cache_data
 def scrap_city(loc1):
-    import os
-
-# Access the GitHub secret
-    endpoint_url = os.getenv("ENDPOINT_URL")
+    endpoint_url = "https://stgpxl9ok3.execute-api.us-east-1.amazonaws.com/dev"
     post = {"loc": str(loc1),"href":"None"}
     response = requests.post(endpoint_url, json=post)
     z=response.text
