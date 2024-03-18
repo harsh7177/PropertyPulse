7550ccb810c3dd636c77f92c07a9ce1df7c8ddb5
Harsh Kandari <harshsingh7177@gmail.com>
2024-03-18 16:37:17 +0530

    Update scrapping.py

--- scrapping.py
+++ scrapping.py
@@ -9,7 +9,7 @@ import json
 
 @st.cache_data
 def scrap_city(loc1):
-    endpoint_url = "https://stgpxl9ok3.execute-api.us-east-1.amazonaws.com/dev"
+    endpoint_url =${{secrets.endpoint_url}}
     post = {"loc": str(loc1),"href":"None"}
     response = requests.post(endpoint_url, json=post)
     z=response.text
